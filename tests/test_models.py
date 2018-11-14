386650957e6ca4f70c44588b9cf824447e349029
Hiroki Kiyohara <hirokiky@gmail.com>
2018-11-14 12:47:32 +0900

    Added python tests.

---
+++ tests/test_models.py
@@ -0,0 +1,25 @@
+from ftree.models import FileTree
+
+
+def test_load():
+    pass
+
+
+def test_load_binary():
+    pass
+
+
+def test_load_readonly():
+    pass
+
+
+def test_load_too_big():
+    pass
+
+
+def test_load_too_deep():
+    pass
+
+
+def test_load_etag():
+    pass
