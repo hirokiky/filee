aca2911ea8f6ca358a20c289daed2e9e00ecfb50
Hiroki Kiyohara <hirokiky@gmail.com>
2018-11-14 12:29:36 +0900

    Refactored.

--- ftree/settings.py
+++ ftree/settings.py
@@ -7,4 +7,4 @@ MAX_SIZE = 1024 * 1024
 MAX_CHILDREN = 50
 MAX_DEPTH = 20
 
-LOAD_FILE_HASHES = os.path.join(os.path.expanduser('~'), '.ftreeloadhashes')
+LOAD_FILE_HASHES = os.path.expanduser('~/.ftreeloadhashes')
