b52a182772e3e17df3e96a359df22ba55a42b4e7
Hiroki Kiyohara <hirokiky@gmail.com>
2018-11-14 13:19:00 +0900

    Improved override settings

--- ftree/testing.py
+++ ftree/testing.py
@@ -1,18 +1,31 @@
-from contextlib import contextmanager
-
 from . import settings
 
 
-@contextmanager
-def override_settings(**new_settings):
-    old_values = {}
+UNSET = object()
 
-    for k, v in new_settings.items():
-        old = getattr(settings, k)
-        old_values[k] = old
-        setattr(settings, k, v)
 
-    yield
+class OverrideSettings:
+    def __init__(self, **new_settings):
+        self.old_values = {}
 
-    for k, v in old_values.items():
-        setattr(settings, k, v)
+        for k, v in new_settings.items():
+            self.set(k, v)
+
+    def set(self, attr, value):
+        old = getattr(settings, attr, UNSET)
+        self.old_values[attr] = old
+        setattr(settings, attr, value)
+
+    def revert(self):
+        for k, v in self.old_values.items():
+            if v is UNSET:
+                delattr(settings, k)
+            else:
+                setattr(settings, k, v)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.revert()
+        return self
