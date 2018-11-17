335bfcfa285b8f70845c506010c558a8d51d5264
Hiroki Kiyohara <hirokiky@gmail.com>
2018-11-17 16:51:02 +0900

    Improved tests

--- tests/test_hashes.py
+++ tests/test_hashes.py
@@ -15,6 +15,7 @@ def test_hash_changed(tmpdir, main_path):
 
     actual = fh.has_changed(main_path, b'This content has been changed\n')
     assert actual
+    assert fh.changed
     assert fh.hashes == {main_path: 'cdff386470e43287d36ca6eb377e93fa'}
 
 
@@ -25,6 +26,7 @@ def test_hash_changed_not_existed(tmpdir, main_path):
 
     actual = fh.has_changed(main_path, b'import this\n')
     assert actual
+    assert fh.changed
     assert fh.hashes == {main_path: '7984fc60703f0e3801005e042bb13c86'}
 
 
@@ -35,4 +37,5 @@ def test_hash_changed_not_changed(tmpdir, main_path):
 
     actual = fh.has_changed(main_path, b'import this\n')
     assert not actual
+    assert not fh.changed
     assert fh.hashes == {main_path: '7984fc60703f0e3801005e042bb13c86'}
