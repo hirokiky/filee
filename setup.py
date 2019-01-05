92e3cb9623399bb0464d61b8c380d10007711fe6
Hiroki KIYOHARA <hirokiky@gmail.com>
2019-01-05 13:31:57 +0900

    Bumped version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='filee',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'Click',
