eb0e3930fcf4aaa4159aacc6d57ea69a124e4ede
Hiroki KIYOHARA <hirokiky@gmail.com>
2019-01-05 14:29:53 +0900

    Bumped version

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import find_packages, setup
 
+readme = open('./README.md', encoding='utf-8').read()
 
 setup(
     name='filee',
-    version='0.0.3',
+    version='0.0.5',
+    description='A command to save / load files with JSON input / output.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'Click',
